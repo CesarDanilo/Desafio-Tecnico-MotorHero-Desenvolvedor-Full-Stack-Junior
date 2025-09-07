d0984343ac43d75080ba4472f4e793941c5b934f
Cesar <billypalacios616@gmail.com>
2025-09-07 01:54:47 -0300

    feat: implementando models e fazendo testes

--- backend/app/routes/quote.py
+++ backend/app/routes/quote.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from app.database import get_connection  # função para pegar a sessão do banco
-from app.models import QuoteRequest  # vamos criar um modelo para resposta
-from app.models import QuoteRequest, QuoteItem, Customer,VehicleConsultRequest  # modelos SQLAlchemy
-# from schemas import QuoteRequest, QuoteItem, Customer, Discount  # seus Pydantic models
+from app.database import get_connection
+from app.models import QuoteRequest
+from app.models import QuoteRequest, QuoteItem, Customer, VehicleConsultRequest
 from typing import List
 
 router = APIRouter()
@@ -12,9 +11,10 @@ router = APIRouter()
 quotes_db: List[QuoteRequest] = []
 vehicle_consults_db: List[VehicleConsultRequest] = []
 
+
 @router.get("/")
 async def get_quote():
-   return quotes_db
+    return quotes_db
 
 
 @router.post("/")
